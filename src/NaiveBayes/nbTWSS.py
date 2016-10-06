bf785e36a82e59ea1ca85ae66b5fa0a762561f23
Tejas <tejas.srinivasan95@gmail.com>
2016-10-07 01:40:00 +0530

    Renamed to nbTWSS.py; camelCase ftw

--- src/NaiveBayes/nbTWSS.py
+++ src/NaiveBayes/nbTWSS.py
@@ -1,8 +1,15 @@
 import pickle 
 import nltk
 
+from unigramFeatures import *
+# from NBClassifier import *
+
+
 def splitTestTrainData(filename, is_twss):
 
+    '''
+    split the training data by using last 100 sentences of given file as test data
+    '''
     train_sents = []
     train_y = []
 
@@ -23,23 +30,46 @@ def splitTestTrainData(filename, is_twss):
 
 
 def twss():
+
+    '''
+    Retrieve training and test sentences and labels
+    Build a vocabulary of words in the training sentences
+    Extract unigram features for training and test sentences
+    Train Naive Bayes classifier using training data
+    Test classifier using test sentences
+    '''
+
     b_train_sents, b_train_y, b_test_sents, b_test_y = splitTestTrainData('../../data/brown_data.pk', 0)
     t_train_sents, t_train_y, t_test_sents, t_test_y = splitTestTrainData('../../data/TWSS_data.pk', 1)
 
-    print len(b_train_sents), len(b_train_y), len(t_train_sents), len(t_train_y)
+    # print len(b_train_sents), len(b_train_y), len(t_train_sents), len(t_train_y)
 
     train_sents = b_train_sents + t_train_sents
     train_y = b_train_y + t_train_y
     test_sents = b_test_sents + t_test_sents 
     test_y = b_test_y + t_test_y
 
+    for sent in train_sents:
+        for w in sent:
+            try:
+                w.decode('ascii')
+            except UnicodeDecodeError:
+                print w, 'was not an ascii-encoded unicode string'
+                print sent
+                print isinstance(w, str), ',', isinstance(w, unicode)
+            else:
+                continue
+
+    print "Building wordset..."	
     wordset = buildWordset(train_sents)
+    print "Extracting features for training data..."
     train_X = extractFeatures(train_sents, wordset)     # Build unigram feature vector for training data
+    print "Extraction features for test data..."
     test_X = extractFeatures(test_sents, wordset)       # Build unigram feature vector for test data
 
-    svm_classifier = classifier(train_X, train_y)
-    predicted_y = predict(classifier, test_y)
-    evaluate(predicted_y, test_y)
+    # svm_classifier = classifier(train_X, train_y)
+    # predicted_y = predict(classifier, test_y)
+    # evaluate(predicted_y, test_y)
 
 if __name__ == '__main__':
     twss()
