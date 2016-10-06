fd0ace7c1dc7c5e009030c2ea01cd55d300bfd25
Tejas <tejas.srinivasan95@gmail.com>
2016-10-07 02:30:55 +0530

    Added comments, removed extra print commands

--- src/NaiveBayes/nbClassifier.py
+++ src/NaiveBayes/nbClassifier.py
@@ -2,6 +2,10 @@ from sklearn.naive_bayes import GaussianNB
 
 def classifier(train_X, train_y):
 
+    '''
+    Trains Naive Bayes classifier using training data
+    '''
+    
     print 'Training Naive Bayes classifier...'
     gnbClassifier = GaussianNB()
     gnbClassifier.fit(train_X, train_y)
@@ -9,11 +13,19 @@ def classifier(train_X, train_y):
 
 def predict(classifier, test_X):
 
+    '''
+    Tests trained NB classifier with test data
+    '''
+
     print 'Predicting from test data...'
     pred_y = classifier.predict(test_X)
     return pred_y
 
 def evaluate(pred_y, test_y, test_sents):
+
+    '''
+    Evaluates predictions of NB classifier
+    '''
 
     print 'Evaluating predictions...'
     TP = 0
@@ -26,14 +38,13 @@ def evaluate(pred_y, test_y, test_sents):
             TP += 1
         elif(pred_val == 1 and test_val == 0):
             FP += 1
-            print 'False Positive:', sent
+            # print 'False Positive:', sent
         elif(pred_val == 0 and test_val == 1):
             FN += 1
-            print 'False Negative:', sent
+            # print 'False Negative:', sent
         elif(pred_val == 0 and test_val == 0):
             TN += 1
 
-    print 'TP:', TP, '\tFP:', FP, '\tFN:', FN, '\tTN:', TN
     precision = float(TP)/(TP+FP)
     recall = float(TP)/(TP+FN)
     F_one = 2*precision*recall/(precision + recall)
