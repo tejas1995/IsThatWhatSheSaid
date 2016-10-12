369839cf568d0ac89aa934be053feaabd405c033
Tejas <tejas.srinivasan95@gmail.com>
2016-10-12 22:19:12 +0530

    Added print statements for TWSS interactor

--- src/twss.py
+++ src/twss.py
@@ -10,17 +10,26 @@ print "B: Scott Von Michael. He's also dumb, but less so."
 
 while(True):
     typ_clf = raw_input('> ')
+
+    # Train classifier for selected Michael Scott
     if(typ_clf == 'A'):
+        print "Waking up Nichael Bott..."
         clf, vocab = nbTWSS.trainTWSS()
         predictor = nbTWSS.predictTWSS
         break
     elif(typ_clf == 'B'):
+        print "Waking up Scott Von Michael..."
         clf, vocab = svmTWSS.trainTWSS()
         predictor = svmTWSS.predictTWSS
         break
 
-while(inp != 'q'):
-    inp = raw_input('> ')
+while(True):
+
+    inp = raw_input('>> ')
+    if inp == 'q':
+        break
+
+    # Predict if input is a TWSS with selected classifier
     twss_res = predictor(clf, vocab, [inp])
     if twss_res[0] == 1:
         print "That's what she said!"
