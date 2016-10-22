4303f69668dd3667b8d21a75cd6054f0d6a62774
Tejas <tejas.srinivasan95@gmail.com>
2016-10-22 20:19:44 +0530

    Read SN and BP lists

--- src/DEviaNT/features.py
+++ src/DEviaNT/features.py
@@ -1,13 +1,27 @@
 import pickle
 import nltk
 import collections
-
-START_SYMBOL = "*"
-STOP_SYMBOL = 'STOP'
+from os import sys, path
 
 
 if __name__ == '__main__' and __package__ is None:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
+def getDeviantFeatures():
 
+    # load SN list
+    SN_filename = path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/data/SN.txt'
+    SN_list = open(SN_filename).readlines()
+    SN_list = [s.strip() for s in SN_list]
+    print SN_list
+
+    # Load BP list
+    BP_filename = path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/data/BP.txt'
+    BP_list = open(BP_filename).readlines()
+    BP_list = [s.strip() for s in BP_list]
+    print BP_list
+
+
+
+getDeviantFeatures()
