66e4dcb40a9e7b61ca1bcb80cd47ed468ffabedc
Tejas <tejas.srinivasan95@gmail.com>
2016-11-02 19:22:45 +0530

    Added SVM classifier and copied svmTWSS to deviantTWSS module

---
+++ src/DEviaNT/deviantTWSS.py
@@ -0,0 +1,96 @@
+import pickle 
+import nltk
+
+from os import sys, path
+from nltk.tokenize.treebank import TreebankWordTokenizer as TWT
+
+from features import *
+from svmClassifier import *
+
+
+if __name__ == '__main__' and __package__ is None:
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from processData import splitTestTrainData
+
+
+def trainTWSS(test=False):
+
+    '''
+    Retrieve training and test sentences and labels
+    Build a vocabulary of words in the training sentences
+    Extract bigram features for training and test sentences
+    Train SVM classifier using training data
+    Test classifier using test sentences
+    '''
+
+    brown_file = path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/data/brown_data.pk'
+    twss_file = path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/data/TWSS_data.pk' 
+    fml_file = path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/data/FML_data.pk'
+    tfln_file = path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/data/TFLN_data.pk' 
+
+    brown_train_sents, brown_train_y, brown_test_sents, brown_test_y = splitTestTrainData(brown_file, 0, 666)
+    twss_train_sents, twss_train_y, twss_test_sents, twss_test_y = splitTestTrainData(twss_file, 1, 2000)
+    fml_train_sents, fml_train_y, fml_test_sents, fml_test_y = splitTestTrainData(fml_file, 0, 666)
+    tfln_train_sents, tfln_train_y, tfln_test_sents, tfln_test_y = splitTestTrainData(tfln_file, 0, 666)
+
+
+    # Create training and test data by joining Brown and TWSS sets
+    train_sents = brown_train_sents + twss_train_sents + fml_train_sents + tfln_train_sents
+    train_y = brown_train_y + twss_train_y + fml_train_y + tfln_train_y
+    test_sents = brown_test_sents + twss_test_sents + fml_test_sents + tfln_test_sents
+    test_y = brown_test_y + twss_test_y + fml_test_y + tfln_test_y
+
+    '''
+    # Use the below for testing with unigram features
+
+    # Build vocalubary of words from training sentences
+    print "Building wordset..."	
+    wordset = buildWordset(train_sents)
+
+    # Build unigram feature vector for training data
+    print "Extracting features for training data..."
+    train_X = extractUnigramFeatures(train_sents, wordset)
+
+    # Build unigram feature vector for test data
+    print "Extracting features for test data..."
+    test_X = extractUnigramFeatures(test_sents, wordset)
+    '''
+
+    # Building bigram vocabulary from training sentences
+    bigramVocab = buildBigramVocab(train_sents)
+ 
+    # Build bigram feature vector for training data
+    train_X = extractBigramFeatures(train_sents, bigramVocab)
+
+    # Train SVM classifier
+    svm_classifier = classifier(train_X, train_y)
+
+    if test is True:
+ 
+        # Build bigram feature vector for test data
+        test_X = extractBigramFeatures(test_sents, bigramVocab)
+
+        # Predict and evaluate results for test data
+        predicted_y = predict(svm_classifier, test_X)
+        evaluate(predicted_y, test_y, test_sents)
+
+    return svm_classifier, bigramVocab
+
+
+def predictTWSS(svm_classifier, bigramVocab, list_sents):
+
+    # Tokenize and preprocess sentences
+    tokenized_sents = []
+    for sent in list_sents:
+        tokenized_sent = TWT().tokenize(sent.strip())
+        tokenized_sent = [w.lower() for w in tokenized_sent]
+        tokenized_sents.append(tokenized_sent)
+
+    input_X = extractBigramFeatures(tokenized_sents, bigramVocab)
+    twss_Y = predict(svm_classifier, input_X)
+    return twss_Y
+
+
+if __name__ == '__main__':
+    trainTWSS(True)
---
+++ src/DEviaNT/svmClassifier.py
@@ -0,0 +1,55 @@
+from sklearn import svm
+
+def classifier(train_X, train_y):
+
+    '''
+    Trains Naive Bayes classifier using training data
+    '''
+    
+    svmClassifier = svm.LinearSVC()
+    svmClassifier.fit(train_X, train_y)
+    return svmClassifier
+
+
+def predict(classifier, test_X):
+
+    '''
+    Tests trained SVM classifier with test data
+    '''
+
+    pred_y = classifier.predict(test_X)
+    return pred_y
+
+
+def evaluate(pred_y, test_y, test_sents):
+
+    '''
+    Evaluates predictions of SVM classifier
+    '''
+
+    print 'Evaluating predictions...'
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+
+    for pred_val, test_val, sent in zip(pred_y, test_y, test_sents):
+        if(pred_val == 1 and test_val == 1):
+            TP += 1
+        elif(pred_val == 1 and test_val == 0):
+            FP += 1
+            # print 'False Positive:', sent
+        elif(pred_val == 0 and test_val == 1):
+            FN += 1
+            # print 'False Negative:', sent
+        elif(pred_val == 0 and test_val == 0):
+            TN += 1
+
+    precision = float(TP)/(TP+FP)
+    recall = float(TP)/(TP+FN)
+    F_one = 2*precision*recall/(precision + recall)
+
+    print 'Precision:', precision
+    print 'Recall:', recall
+    print 'F1 Score:', F_one
+
